1a71ab5634b9434e242e8792de163fbc127552ce
csslick <digitone@naver.com>
2021-05-16 22:08:33 +0900

    first list view

--- src/App.js
+++ src/App.js
@@ -3,31 +3,30 @@ import './App.css'
 import Pokemon from './components/Pokemon'
 
 function App() {
-  const [pokemons, setPokemons] = useState([])
   const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/?limit=20');
   const [pokemonObj, setPokemonObj] = useState([]);
   
   const getPokemons = async () => {
+    // 1.pokemon 목록 조회
     const res = await fetch(url);
     const data = await res.json()
     // console.log(data);
-    setPokemons(data.results);
 
-    // next data url
-    setUrl(data.next);
+    setUrl(data.next);  // next data url
 
-    createPokemon(data.results);
+    // 2.조회된 url에 링크된 포케몬 정보 조회
+    const createPokemon = () => {
+      const _url = 'https://pokeapi.co/api/v2/pokemon/';
+      setPokemonObj([]);
+      data.results.forEach(async (pokemon) => {
+        const res = await fetch(`${_url}${pokemon.name}`)
+        const data = await res.json();
+        setPokemonObj(current => [...current, data])
+      })
+    }
+
+    createPokemon();
     console.log('pokemonObj = ', pokemonObj)
-  }
-
-  const createPokemon = () => {
-    const _url = 'https://pokeapi.co/api/v2/pokemon/';
-    setPokemonObj([]);
-    pokemons.forEach(async (pokemon) => {
-      const res = await fetch(`${_url}${pokemon.name}`)
-      const data = await res.json();
-      setPokemonObj(current => [...current, data])
-    })
   }
 
   useEffect(()=> {
