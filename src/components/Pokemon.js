9f8ee5f100a4a0a27c2856012916533ffe2f5b45
csslick <digitone@naver.com>
2021-05-16 22:19:39 +0900

    111

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,8 +4,8 @@ export default function Pokemon({id, name, image, type}) {
   return (
     <div className='pokemon-info'>
       <span className="pokemon-id">#{id}</span>
-      <h4>{name}</h4>
       <img src={image} alt={name} />
+      <h4>{name}</h4>
       <span className="type">Type: {type}</span>
     </div>
   )
